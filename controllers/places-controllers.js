ed47c1f2e0c544497ad7bfdc2f9f5d4c33c76279
Stewart Newnham <git@newnham.com>
2021-08-08 13:05:14 +0800

    improved security so that the creaorr id is no not passed in req and re

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -74,7 +74,7 @@ const createPlace = async (req, res, next) => {
     return next(new HttpError("Invalid Inputs", 402));
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -91,12 +91,12 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     const error = new HttpError("Creating Place Failed", 500);
     return next(error);
